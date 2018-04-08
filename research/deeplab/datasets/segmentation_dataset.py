f2d44c07e174bc67712945fd00576adf86e35a21
wonderit <kws42590@gmail.com>
2018-04-08 22:06:31 +0900

    ade20k update

--- research/deeplab/datasets/segmentation_dataset.py
+++ research/deeplab/datasets/segmentation_dataset.py
@@ -14,28 +14,21 @@
 # ==============================================================================
 
 """Provides data from semantic segmentation datasets.
-
 The SegmentationDataset class provides both images and annotations (semantic
 segmentation and/or instance segmentation) for TensorFlow. Currently, we
 support the following datasets:
-
 1. PASCAL VOC 2012 (http://host.robots.ox.ac.uk/pascal/VOC/voc2012/).
-
 PASCAL VOC 2012 semantic segmentation dataset annotates 20 foreground objects
 (e.g., bike, person, and so on) and leaves all the other semantic classes as
 one background class. The dataset contains 1464, 1449, and 1456 annotated
 images for the training, validation and test respectively.
-
 2. Cityscapes dataset (https://www.cityscapes-dataset.com)
-
 The Cityscapes dataset contains 19 semantic labels (such as road, person, car,
 and so on) for urban street scenes.
-
 References:
   M. Everingham, S. M. A. Eslami, L. V. Gool, C. K. I. Williams, J. Winn,
   and A. Zisserman, The pascal visual object classes challenge a retrospective.
   IJCV, 2014.
-
   M. Cordts, M. Omran, S. Ramos, T. Rehfeld, M. Enzweiler, R. Benenson,
   U. Franke, S. Roth, and B. Schiele, "The cityscapes dataset for semantic urban
   scene understanding," In Proc. of CVPR, 2016.
@@ -85,10 +78,23 @@ _PASCAL_VOC_SEG_INFORMATION = DatasetDescriptor(
     ignore_label=255,
 )
 
+# These number (i.e., 'train'/'test') seems to have to be hard coded
+# You are required to figure it out for your training/testing example.
+# Is there a way to automatically figure it out ?
+_ADE20K_INFORMATION = DatasetDescriptor(
+    splits_to_sizes = {
+        'train': 20210,  # num of samples in images/training
+        'val': 2000,  # num of samples in images/validation
+    },
+    num_classes=150,
+    ignore_label=255,
+)
+
 
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
+    'ade20k': _ADE20K_INFORMATION,
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
@@ -101,15 +107,12 @@ def get_cityscapes_dataset_name():
 
 def get_dataset(dataset_name, split_name, dataset_dir):
   """Gets an instance of slim Dataset.
-
   Args:
     dataset_name: Dataset name.
     split_name: A train/val Split name.
     dataset_dir: The directory of the dataset sources.
-
   Returns:
     An instance of slim Dataset.
-
   Raises:
     ValueError: if the dataset_name or split_name is not recognized.
   """
